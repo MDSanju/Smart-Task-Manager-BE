6844066bfb168f383c163f7fddb383f891d37bbb
MDSanju <mdsanju387@gmail.com>
2025-11-20 07:15:09 +0600

    Project Setup Done

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use("/api/auth", require("./routes/auth"));
+
+// Health
+app.get("/", (req, res) => res.send("API running"));
+
+// Start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
